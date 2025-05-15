cefc35a0fc733b95066fd1c01f6d7a77ccccabc2
ebin <109602426+ebinjosey@users.noreply.github.com>
2025-05-15 11:35:08 +0400

    Update script.js

--- script.js
+++ script.js
@@ -73,6 +73,7 @@ document.getElementById('game').addEventListener('keyup', ev => {
         if (curentLetter && isFirstLetter) {
             removeClass(currentWord, 'current');
             addClass(currentWord.previousSibling, 'current');
+            removeClass(currentLetter, '.current');
         }
 
     }
