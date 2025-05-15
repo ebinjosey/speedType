6f3a7a3553f3873bf4c313cc4d842f68d5d4a454
ebin <109602426+ebinjosey@users.noreply.github.com>
2025-05-15 11:15:25 +0400

    Update script.js

--- script.js
+++ script.js
@@ -66,6 +66,13 @@ document.getElementById('game').addEventListener('keyup', ev => {
         }
         addClass(currentWord.nextSibling.firstChild, 'current');
     }
+
+    const nextLetter = document.querySelector('.letter.current');
+    const cursor = document.getElementById('cursor');
+    if (nextLetter) {
+        cursor.style.top = nextLetter.getBoundingClientRect().top + 2 +'px';
+        cursor.style.left = nextLetter.getBoundingClientRect().left + 0.2 + 'px';
+    }
 })
 
 newGame()
